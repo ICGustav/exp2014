023fb850a4d9bf47d89c70a477619e27ad1905c2
Gustav <gustav.hlavac@gmail.com>
2014-10-15 15:37:39 +0200

    Commiting Project exp2014 after session with routes of users params done right

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res) {
   console.log(req.query);
 });
 
+router.get('/:id', function(req, res) {
+  res.send(req.params, 200);
+  console.log(req.params);
+});
+
 router.post('/', function (req, res) {
 	console.log(req.query);
 	res.send(200);
