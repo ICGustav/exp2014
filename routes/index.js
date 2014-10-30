aef8b866e95c98f6f7badfff8da6f8eb4ed03e76
Gustav <gustav.hlavac@gmail.com>
2014-10-30 15:44:56 +0100

    Updated index routes for load JSON data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var myData = require('./myData.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', myData);
+  console.log(myData);
 });
 
 module.exports = router;
